9be6b2fcb4bf2ac52e9b360815b30d4242738e7a
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2023-01-02 15:14:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,8 +17,44 @@ function takeSnapshot(){
 }
 
 console.log("ml5 version:",ml5.version)
-classiflier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ykEQ3UWQJ/model.json",modelloaded)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/ykEQ3UWQJ/model.json",modelloaded)
 
 function modelloaded(){
     console.log("model is loaded")
+}
+function check(){
+    img = document.getElementById("resultimg")
+    classifier.classify(img,gotresult)
+}
+function gotresult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else if(results){
+        console.log(results)
+        prediction = results[0].label;
+        document.getElementById("result_emotion_name").innerHTML = prediction;
+        speak();
+        if(prediction == "Amazing"){
+            document.getElementById("update_emoji").innerHTML = "&#128076;"
+        } 
+        else if(prediction == "victory"){
+            document.getElementById("update_emoji").innerHTML = "&#9996;"
+        } 
+        else if(prediction == "Aggre"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;"
+        } 
+        else if(prediction == "just a part"){
+            document.getElementById("update_emoji").innerHTML = "&#128077;"
+        }
+        else if(prediction == "Swag"){
+            document.getElementById("update_emoji").innerHTML = "&#129304;"
+        }
+        else if(prediction == "fingers crossed"){
+            document.getElementById("update_emoji").innerHTML = "&#129310"
+        }
+        else if(prediction == "Disaggre"){
+            document.getElementById("update_emoji").innerHTML = "&#128078;"
+        }
+    }
 }
